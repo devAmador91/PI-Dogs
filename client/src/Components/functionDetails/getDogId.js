cf8bb4ba5253c57500ffa68e966037f2772a389f
RepasoHenry <devWarrior7@protonmail.com>
2022-05-13 15:37:05 -0500

    deploy fix bug

--- client/src/Components/functionDetails/getDogId.js
+++ client/src/Components/functionDetails/getDogId.js
@@ -2,7 +2,7 @@ import { urlLocal } from "../../Actions";
 import { urlBack } from "../../Actions";
 
 export const getDogId = async (id, setDog) => {
-    const response = await fetch(`${urlLocal || urlBack}dogs/${id}`);
+    const response = await fetch(`${urlBack || urlLocal}dogs/${id}`);
     const dogJson = await response.json();
     setDog(dogJson);
   };
