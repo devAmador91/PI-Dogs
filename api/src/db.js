36b3b8575f56d924134266c051647182a05c2508
Amador Aguilar <90819559+devAmador91@users.noreply.github.com>
2022-11-30 19:10:24 +0000

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
 const modelDog = require('./models/Dog.js');
 const modelTemperament = require('./models/Temperament.js');
 
@@ -9,7 +9,7 @@ const sequelize = process.env.NODE_ENV === "production"
     database: DB_NAME,
     dialect: "postgres",
     host: DB_HOST,
-    port: 5432,
+    port: DB_PORT,
     username: DB_USER,
     password: DB_PASSWORD,
     pool: {
